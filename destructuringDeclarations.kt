f2a3d916651c9a7960a70ec56dcd0567893d7369
ProstoBorov01 <146583339+ProstoBorov01@users.noreply.github.com>
2023-10-18 13:52:44 +0300

    Create destructuringDeclarations.kt
    destructuring declarations в Kotlin

---
+++ destructuringDeclarations.kt
@@ -0,0 +1,57 @@
+
+class Student(val name: String, val level: Any) {
+    operator fun component1() = name
+    operator fun component2() = level
+}
+
+data class User (
+    val id: Int,
+    val name: String,
+    val age: Int
+)
+
+fun main() {
+
+    // destructuring declarations
+
+    val person = User(1, "Sergey", 18)
+    val student = Student("Sergey", 2)
+    val array = listOf("first", 2, true)
+    val array2 = listOf(person)
+    val (id1, name1, age1) = person
+    val (studentName ,level) = student
+    val (_, _, flag) = array
+
+    for ((id, name, age) in array2) { println("$id $name $age") }
+
+    println(level)
+    println(flag)
+}
+class Student(val name: String, val level: Any) {
+    operator fun component1() = name
+    operator fun component2() = level
+}
+
+data class User (
+    val id: Int,
+    val name: String,
+    val age: Int
+)
+
+fun main() {
+
+    // destructuring declarations
+
+    val person = User(1, "Sergey", 18)
+    val student = Student("Sergey", 2)
+    val array = listOf("first", 2, true)
+    val array2 = listOf(person)
+    val (id1, name1, age1) = person
+    val (studentName ,level) = student
+    val (_, _, flag) = array
+
+    for ((id, name, age) in array2) { println("$id $name $age") }
+
+    println(level)
+    println(flag)
+}
